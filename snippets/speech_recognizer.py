e1966c5f0a09f1ef67153ce678068cfdcbf14b46
atsss <atsushi0623itoh@gmail.com>
2024-05-15 07:47:02 +0100

    Add adjust_for_ambient_noise function to adjust surroundings noise

--- snippets/speech_recognizer.py
+++ snippets/speech_recognizer.py
@@ -2,9 +2,12 @@ import speech_recognition as sr
 
 # obtain audio from the microphone
 r = sr.Recognizer()
-with sr.Microphone() as source:
+with sr.Microphone(device_index=0) as source:
+    print("Listening surrounding!")
+    r.adjust_for_ambient_noise(source,duration=5)
     print("Say something!")
     audio = r.listen(source)
+    print("Understanting!")
 
 # recognize speech using whisper
 try:
